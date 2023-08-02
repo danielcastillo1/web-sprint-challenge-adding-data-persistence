b1719b456432c723c1697c4e4c117cbbdb5be7f0
Daniel Castillo <danielcastillo89@icloud.com>
2023-08-01 21:08:19 -0500

    resource router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,33 @@
 // build your `/api/resources` router here
+const router = require("express").Router();
+const Resource = require("./model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await Resource.getAll();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const data = await Resource.addNew(req.body);
+    res.status(201).json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(500).json({
+    customMessage:
+      "something went wrong inside the Resources router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
