9d79625a67ea450f53cbbefc40651e93ae60f1a9
Daniel Castillo <danielcastillo89@icloud.com>
2023-08-01 21:07:40 -0500

    resource

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,29 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig");
+const xModel = "resources";
+
+async function getAll() {
+  const data = await db(`${xModel}`);
+
+  return data;
+}
+
+async function getById(id) {
+  const itemById = await db(`${xModel}`)
+    .where("resource_id", id)
+    .first();
+
+  return itemById;
+}
+
+async function addNew(newData) {
+  const id = await db(`${xModel}`).insert(newData);
+
+  return getById(id);
+}
+
+module.exports = {
+  getAll,
+  addNew,
+  getById,
+};
