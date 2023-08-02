2b8438c704abb65af6b7ea2f7f27f9c7ff11671c
Daniel Castillo <danielcastillo89@icloud.com>
2023-08-01 21:06:26 -0500

    model.js

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,43 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+const xModel = "projects";
+
+async function getAll() {
+  const data = await db(`${xModel}`);
+
+  data.map((element) => {
+    if (element.project_completed === 1) {
+      element.project_completed = true;
+    } else {
+      element.project_completed = false;
+    }
+  });
+
+  return data;
+}
+
+async function getById(id) {
+  const itemById = await db(`${xModel}`)
+    .where("project_id", id)
+    .first();
+
+  if (itemById.project_completed === 1) {
+    itemById.project_completed = true;
+  } else {
+    itemById.project_completed = false;
+  }
+
+  return itemById;
+}
+
+async function addNew(newData) {
+  const id = await db(`${xModel}`).insert(newData);
+
+  return getById(id);
+}
+
+module.exports = {
+  getAll,
+  addNew,
+  getById,
+};
